32155c84c985afb82bd3f5332c7e7c95b381a788
DESKTOP-IDGPTHF\Admin <dkruhlikov@gmail.com>
2018-03-13 17:59:22 +0200

    first

---
+++ js/common.js
@@ -0,0 +1,56 @@
+$(function() {
+
+	$('.toggle-fix').click(function(){
+		$('.fix-mnu').toggleClass('active-fix');
+	});
+
+	$('.toggle-nav').click(function(){
+		$('.top-mnu').slideToggle();
+	});
+
+	$('.lightslider').lightSlider({
+		item: 2,
+		pager: false,
+		responsive: [
+			{
+				breakpoint: 480,
+				settings: {
+					item: 1
+				}
+			}
+		]
+	});
+
+
+	$("form").validate({
+		messages: {
+			name: "Введите имя",
+			email: {
+				required: "Введите мейл",
+				email: "Введите валидный мейл"
+			}
+		},
+		submitHandler: function(form){
+			var msg = $(form).find('input, textarea');
+            $.ajax({
+                type: 'POST',
+                url: 'contact.php',
+                data: msg,
+                success: function (data) {
+                	if(data == '1'){
+                		$("a[href='#success']").click();
+                		form.reset();
+                	}else{
+                		alert("error");
+                	}
+                    console.log(data);
+                },
+                error: function (xhr, str) {
+                    alert(xhr.responseCode);
+                }
+            });
+		}
+
+	});
+
+});
